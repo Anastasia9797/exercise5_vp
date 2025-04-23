0507cf51d257a8bf578c1f6c57dccc684420396d
Anastasia9797 <n6609400@gmail.com>
2025-04-23 11:06:46 +0300

    Example 3

--- exercise5_vp/MainWindow.xaml.cs
+++ exercise5_vp/MainWindow.xaml.cs
@@ -20,5 +20,24 @@ namespace exercise5_vp
         {
             InitializeComponent();
         }
+
+        private void btnPreviousTab_Click(object sender, RoutedEventArgs e)
+        {
+            int nexIndex = tcSample.SelectedIndex - 1;
+            if (nexIndex < 0) nexIndex = tcSample.Items.Count - 1;
+            tcSample.SelectedIndex = nexIndex;
+        }
+
+        private void btnNextTab_Click(object sender, RoutedEventArgs e)
+        {
+            int nexIndex = tcSample.SelectedIndex + 1;
+            if (nexIndex >= tcSample.Items.Count) nexIndex = 0;
+            tcSample.SelectedIndex = nexIndex;
+        }
+
+        private void btnSelectedTab_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Selected tab: " + (tcSample.SelectedItem as TabItem).Header);
+        }
     }
 }
